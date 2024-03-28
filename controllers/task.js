cbff25f71830f24d1c4eff8c20283250da0984c0
kezagiselle <kezagiselle776@gmail.com>
2024-03-28 11:39:41 +0200

    continuing

--- controllers/task.js
+++ controllers/task.js
@@ -28,11 +28,25 @@ const getTasks = async (req, res) => {
     } catch (error){
         res.status(500).json({msg: error})
     }
-  
+ 
 }
 
-const updateTasks = (req, res) => {
-    res.send('Update a task')
+const updateTasks = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findByIdAndUpdate({_id:taskID},req.body,{
+            new: true,
+            runValidators: true,
+        })
+
+        if(!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({id:taskID,data:req.body})
+    } catch (error){
+        res.status(500).json({msg: error})
+    }
+
 }
 
 const deleteTasks = async (req, res) => {
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const tasks =  require('./routes/task')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 //middleware
+//app.use(express.static('./))
 app.use(express.json() )
 //routes
 app.get('/hello',(req,res) => {
@@ -27,3 +28,8 @@ const start = async () => {
 start()
 //module.exports = connectDB
 
+//app.get('/api/v1/tasks') get all tasks
+//app.post('/api/v1/tasks') create a new task
+//app.get('/api/v1/tasks/:id') get a single task
+//app.patch('/api/v1/tasks/:id') get a single task
+//app.delete('/api/v1/tasks/:id) delete task
